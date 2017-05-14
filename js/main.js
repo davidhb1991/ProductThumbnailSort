d1728a0fd6c355d8b0e45af40b5bedd4cf5857d1
DavidBirchall <dhbirchall@me.com>
2017-05-14 18:17:36 +0100

    Added search box and sort button functionality.

--- js/main.js
+++ js/main.js
@@ -2,11 +2,23 @@ var app = angular.module('ProductThumbnailSort',[])
 
 .controller('MainCtrl', ['$scope', '$http', function($scope, $http){
    
-    // Get the JSON file with the data and store it in products
+    // Get the JSON file with the data and store it in products.
     $http.get('products/products.json').then(function(response){
         $scope.products = response.data;
     });
+    // Change the default sort order to whatever button is active.
+    $scope.myOrderBy = 'rating';
+    // Create a function to change the sort order to whatever is passed in from the DOM.
+    $scope.orderByMe = function(x) {
+        $scope.myOrderBy = x;
+        console.log('Accessed orderByMe and the user passed ' + x + ' as the property to sort by.');
+    }
+    // Change the state of the sort buttons when they are pressed.
+    $('.button--sort-button').click(function(){
+        $('.button--sort-button').removeClass('button--active');
+        $(this).addClass('button--active');
+    });
 
-    console.log($scope);
+    console.log('Loaded MainCtrl');
  
 }]);
