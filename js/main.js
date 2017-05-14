8540e8b1673090599583165f5ef2907ee9feb88b
DavidBirchall <dhbirchall@me.com>
2017-05-14 19:57:56 +0100

    started prep for animation

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-var app = angular.module('ProductThumbnailSort',[])
+// Define a new module for the app and add the animation module as a dependency.
+var app = angular.module('ProductThumbnailSort',['ngAnimate'])
 
 .controller('MainCtrl', ['$scope', '$http', function($scope, $http){
    
@@ -22,3 +23,77 @@ var app = angular.module('ProductThumbnailSort',[])
     console.log('Loaded MainCtrl');
  
 }]);
+
+
+
+
+
+// study below
+
+angular.module('StackApp', []).controller('MainCtrl', function($scope) {
+  'use strict';
+
+  $scope.reverse = 'false';
+
+  $scope.myList = [{
+    id: 0,
+    text: 'HTML5 Boilerplate'
+  }, {
+    id: 1,
+    text: 'AngularJS'
+  }, {
+    id: 2,
+    text: 'Karma'
+  }, {
+    id: 3,
+    text: 'Hello'
+  }, {
+    id: 4,
+    text: 'World'
+  }, {
+    id: 5,
+    text: 'How'
+  }];
+
+  $scope.$watch('reverse', function() {
+    $scope.setOrder();
+  });
+
+  $scope.setOrder = function() {
+
+    if ($scope.reverse === 'random') {
+
+      var t = [];
+
+      for (var i = 0; i < $scope.myList.length; i++) {
+        var r = Math.floor(Math.random() * $scope.myList.length);
+        while (inArray(t, r)) {
+          r = Math.floor(Math.random() * $scope.myList.length);
+        }
+        t.push(r);
+        $scope.myList[i].order = r;
+      }
+
+    } else {
+
+      for (var i = 0; i < $scope.myList.length; i++) {
+        if ($scope.reverse === 'false') {
+          $scope.myList[i].order = i;
+        } else {
+          $scope.myList[i].order = ($scope.myList.length - 1 - i);
+        }
+      }
+    }
+  };
+
+  function inArray(a, value) {
+    for (var i = 0; i < a.length; i++) {
+      if (a[i] === value) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+});
+
